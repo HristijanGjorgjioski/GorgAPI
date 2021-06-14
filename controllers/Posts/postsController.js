784631ff256f38521ae10a3e3eb801116074bbd0
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-14 08:12:35 +0200

    Created deletePost and createPost controllers

--- controllers/Posts/postsController.js
+++ controllers/Posts/postsController.js
@@ -7,9 +7,24 @@ exports.getPosts = async (req, res) => {
 }
 
 exports.createPost = async (req, res) => {
+    const { description, selectedFile, creator, name } = req.body;
 
+    try {
+        const result = await Post.create({ creator, name, description, photo: selectedFile });
+
+        res.status(201).json({ result });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
 }
 
 exports.deletePost = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully." });
 }
