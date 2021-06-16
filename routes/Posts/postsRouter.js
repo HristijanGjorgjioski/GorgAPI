2fb7cd9a8b9cfa7593eec51ec5666a535d0558c2
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-16 20:33:31 +0200

    Created Post routes with auth middleware

--- routes/Posts/postsRouter.js
+++ routes/Posts/postsRouter.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
-const router = express.Router();
-
 const { getPosts, getPostsBySearch, getPost, createPost, updatePost, likePost, deletePost } = require('../../controllers/Posts/postsController');
+
+const router = express.Router();
+const auth = require('../../middlewares/authMiddleware');
 
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.get('/:id', getPost);
+
+router.post('/', auth,  createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 module.exports = router;
